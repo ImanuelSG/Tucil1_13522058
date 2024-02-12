ff70fe5a843a37be3cd6c97671bbe96648aa33e6
ImanuelGirsang <71023099+ImanuelSG@users.noreply.github.com>
2024-02-13 05:08:26 +0700

    fix : try fix vercel

--- src/app/api/main/route.ts
+++ src/app/api/main/route.ts
@@ -3,33 +3,30 @@ import { spawn } from "child_process";
 
 export const POST = async (req: NextRequest): Promise<void | Response> => {
   const data = JSON.stringify(await req.json()); // Convert data to JSON string
-  // return new Promise((resolve, reject) => {
-  //   const dir = process.cwd() + "/tes.exe" // Path to the C++ executable
-  //   console.log(dir);
-  //   const cppProcess = spawn(dir, [data]);
-  //   let output = "";
-  //   let error = "";
+  return new Promise((resolve, reject) => {
+    const cppProcess = spawn("src/app/api/main/tes.exe", [data], );
+    let output = "";
+    let error = "";
 
-  //   cppProcess.stdout.on("data", (data) => {
-  //     output += data.toString();
-  //   });
+    cppProcess.stdout.on("data", (data) => {
+      output += data.toString();
+    });
 
-  //   cppProcess.stderr.on("data", (data) => {
-  //     error += data.toString();
-  //   });
+    cppProcess.stderr.on("data", (data) => {
+      error += data.toString();
+    });
 
-  //   cppProcess.on("close", (code) => {
-  //     if (code !== 0) {
-  //       output = error;
-  //     }
+    cppProcess.on("close", (code) => {
+      if (code !== 0) {
+        output = error;
+      }
 
-  //     const response = NextResponse.json({ output }, { status: 200 });
-  //     resolve(response);
-  //   });
+      const response = NextResponse.json({ output }, { status: 200 });
+      resolve(response);
+    });
 
-  //   cppProcess.on("error", (err) => {
-  //     reject(NextResponse.json({ data }, { status: 500 })); // Reject the promise if there's an error with the child process
-  //   });
-  // });
-  return NextResponse.json({ message: `${process.cwd()}` }, { status: 200 });
+    cppProcess.on("error", (err) => {
+      reject(NextResponse.json({ data }, { status: 500 })); // Reject the promise if there's an error with the child process
+    });
+  });
 };
